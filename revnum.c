23f5290f3ed79584ce9f8de7efdce26525abdf1a
P.Rushika <93852629+Rushika27@users.noreply.github.com>
2022-04-03 11:19:42 +0530

    Create revnum.c

---
+++ revnum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int main() {
+	int a, rem, sum = 0;;
+	scanf("%d", &a);
+	while (a--) {
+
+		int n;
+		scanf("%d", &n);
+		while(n!=0) {
+			rem = n % 10;
+			sum = sum * 10 + rem;
+			n = n / 10;
+		}
+		printf("%d\n", sum);
+		sum = 0;
+
+		}
+	return 0;
+
+	}
